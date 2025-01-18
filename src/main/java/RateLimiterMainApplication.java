f2fe879fdf82618f194d66a097c4e700fea06ab7
Vinit Kumar <63940739+router19@users.noreply.github.com>
2025-01-18 22:09:18 +0530

    Update RateLimiterMainApplication.java

--- src/main/java/RateLimiterMainApplication.java
+++ src/main/java/RateLimiterMainApplication.java
@@ -3,9 +3,9 @@ import java.util.concurrent.TimeUnit;
 public class RateLimiterMainApplication {
     private static  final int MAX_TOKEN = 5;
     public static void main(String[] args) throws InterruptedException{
-        //refill rate 1 token per sec
+        //refill rate 3 token per sec
 
-        //means rate limiting 1 request per second
+        //means rate limiting 3 request per second
         RateLimiter rateLimiter = new RateLimiter(MAX_TOKEN, TimeUnit.SECONDS.toNanos(1),3);
         String clientId = "client1";
         for(int requestNumber =0; requestNumber < 13; requestNumber++){
